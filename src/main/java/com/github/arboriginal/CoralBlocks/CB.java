7bf07d4ba40ae08376e28086c10427fd4fce5813
arboriginal <1117167+arboriginal@users.noreply.github.com>
2019-12-17 01:40:45 +0100

    Version 1.1
    - Now when a player break an alive coral block, an alive coral block
      will be dropped instead of a dead one
    - (internal) Using material list instead of the CORAL_BLOCKS tag,
      as it is not a MC tag but a Spigot custom one, its content is
      described by "Vanilla block tag representing all coral blocks"
      which is supposed to correspond to minecraft:coral tag,
      except it differ a bit (no "minecraft:dead_coral" in it).
      To be sure it will not be changed in the future and break the plugin,
      I prefer then to use a specific list instead of this tag.

--- src/main/java/com/github/arboriginal/CoralBlocks/CB.java
+++ src/main/java/com/github/arboriginal/CoralBlocks/CB.java
@@ -1,33 +1,46 @@
 package com.github.arboriginal.CoralBlocks;
 
-import java.util.Set;
+import java.util.Arrays;
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.Tag;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CB extends JavaPlugin implements Listener {
-    private final MetadataValue v = new FixedMetadataValue(this, true);
-    private final String        k = getClass().getCanonicalName();
-    private Set<Material>       c;
+    private final String         k = getClass().getCanonicalName();
+    private final MetadataValue  f = new FixedMetadataValue(this, true);
+    private final List<Material> c = Arrays.asList(Material.BUBBLE_CORAL_BLOCK, Material.BRAIN_CORAL_BLOCK,
+            Material.FIRE_CORAL_BLOCK, Material.HORN_CORAL_BLOCK, Material.TUBE_CORAL_BLOCK);
 
     @Override
     public void onEnable() {
         super.onEnable();
-        c = Tag.CORAL_BLOCKS.getValues();
         Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    private void onBlockBreak(BlockBreakEvent e) {
+        Block    b = e.getBlock();
+        Material t = b.getType();
+        if (!c.contains(t) || !e.isDropItems()) return;
+        b.removeMetadata(k, this);
+        b.getWorld().dropItemNaturally(b.getLocation(), new ItemStack(t));
+        e.setDropItems(false);
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     private void onBlockPlace(BlockPlaceEvent e) {
-        if (c.contains(e.getBlock().getType()) && e.getPlayer().isSneaking()) e.getBlock().setMetadata(k, v);
+        if (c.contains(e.getBlock().getType()) && e.getPlayer().isSneaking()) e.getBlock().setMetadata(k, f);
     }
 
     @EventHandler(ignoreCancelled = true)
